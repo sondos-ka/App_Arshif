f6b971369db0fbeb1ae4bd03e0ee32c67ad04347
sondos <sondoskannous@gmail.com>
2020-09-07 11:18:45 +0300

    first commit

---
+++ app/src/main/java/com/example/arshif/AccountObject.java
@@ -0,0 +1,24 @@
+package com.example.arshif;
+
+public class AccountObject {
+
+
+    int Id;
+    int TypeCost;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public int getTypeCost() {
+        return TypeCost;
+    }
+
+    public void setTypeCost(int typeCost) {
+        TypeCost = typeCost;
+    }
+}
